c27f06175bc22dab399b16863f06407f4b0b9ddd
Angela Hao Jiang <ahjiang@ops.orca.pdl.cmu.edu>
2017-04-24 12:00:48 -0400

    Bugs are less frequent Added _make_predict_function Use graph.as_default() Initialized local and global variables

--- inference/inference.py
+++ inference/inference.py
@@ -1,11 +1,10 @@
 import threading
 import datetime
-import sys
-sys.path.append('../util')
-import DataSet
 import json
 import tensorflow as tf
 
+from keras import backend as K
+from keras.backend.tensorflow_backend import get_session
 from keras.applications.inception_v3 import InceptionV3
 from keras.applications.vgg16 import VGG16
 from keras.models import Model, model_from_json
@@ -29,10 +28,9 @@ def build_model_vgg(nb_classes, num_apps):
 
     # this is the model we will train
     model = Model(input=base_model.input, outputs=top_layers)
-
-    print "starting model compile"
+    model._make_predict_function()
     compile(model)
-    print "model compile done"
+
     return model
 
 def build_model(nb_classes, num_apps):
@@ -51,10 +49,9 @@ def build_model(nb_classes, num_apps):
 
     # this is the model we will train
     model = Model(input=base_model.input, outputs=top_layers)
-
-    print "starting model compile"
+    model._make_predict_function()
     compile(model)
-    print "model compile done"
+
     return model
 
 def compile(model):
@@ -62,54 +59,51 @@ def compile(model):
 
 graph = None # https://github.com/fchollet/keras/issues/2397
 
-def benchmark(dataset_dir, num_apps, batch_size, shared=False):
-    data = DataSet.DataSet(dataset_dir, 224)
+def benchmark(data, num_apps, batch_size, shared=False):
 
-    '''
+    # To warm up some caches or something
+    model = build_model(data.nb_classes, num_apps)
+    model.predict(data.X_train, batch_size=batch_size)
+
     if shared:
         model = build_model(data.nb_classes, num_apps)
+        graph = tf.get_default_graph()
+        t = threading.Thread(target = inference_async, args = [model, data, batch_size, graph])
         start = datetime.datetime.now()
-        t = threading.Thread(target = inference_async, args = [model, data])
         t.start()
         t.join()
         end = datetime.datetime.now()
         '''
-    # To warm up some caches or something
-    model = build_model(data.nb_classes, num_apps)
-    model.predict(data.X_train, batch_size=batch_size)
-    
-    if shared:
-        model = build_model(data.nb_classes, num_apps)
         start = datetime.datetime.now()
         model.predict(data.X_train, batch_size=batch_size)
         end = datetime.datetime.now()
+        '''
     else:
-        models = []
-        for x in range(1, num_apps + 1):
-            model = build_model(data.nb_classes, 1)
-            models.append(model)
-            global graph
-            graph = tf.get_default_graph()
         threads = []
-        for x, model in zip(range(1, num_apps + 1), models):
-            start = datetime.datetime.now()
-            print "[LOG] Creating thread", x
-            t = threading.Thread(target = inference_async, args = [model, data, batch_size])
+        model = build_model(data.nb_classes, 1)
+        graph = tf.get_default_graph()
+        get_session().run(tf.global_variables_initializer())
+        get_session().run(tf.local_variables_initializer())
+        for x in range(1, num_apps + 1):
+            t = threading.Thread(target = inference_async, args = [model, data, batch_size, graph])
+            threads.append(t)
+        start = datetime.datetime.now()
+        for x, t in zip(range(1, num_apps + 1), threads):
             print "[LOG] Starting thread", x
             t.start()
-            threads.append(t)
         for t in threads:
             t.join()
         end = datetime.datetime.now()
     elapsed = end - start
     fps = len(data.X_train) / float(elapsed.seconds)
-    print "Shared:", shared, "Num apps: ", num_apps, fps, "fps"
+    print "Shared:", shared, "Num apps:", num_apps, "Elapsed:", elapsed, "Speed:", fps, "fps"
     return fps
 
-def inference_async(model, data, batch_size):
+def inference_async(model, data, batch_size, graph):
+    #tf_session = K.get_session()
+    #with tf_session.as_default():
+    #g = tf.Graph()
+    #print "[INFO 2]", g, graph
     with graph.as_default():
         model.predict(data.X_train, batch_size=batch_size)
-
-    
-
 
