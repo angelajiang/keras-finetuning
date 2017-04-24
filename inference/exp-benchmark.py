f597680fa7a0821f867783ee496f328f99adf04e
Angela Hao Jiang <ahjiang@ops.orca.pdl.cmu.edu>
2017-04-24 12:01:20 -0400

    Get data in exp, condense

--- inference/exp-benchmark.py
+++ inference/exp-benchmark.py
@@ -1,22 +1,18 @@
-import inference
 import sys
+sys.path.append('../util')
+import DataSet
+import inference
 
 def run(dataset_dir, batch_size, num_apps_range, num_apps_stride, output_dir):
     xs = [x for x in range(1, num_apps_range + 1, num_apps_stride)]
+    data = DataSet.DataSet(dataset_dir, 224)
     shared = []
     not_shared = []
-    try:
-        for num_apps in xs:
-            fps = inference.benchmark(dataset_dir, num_apps, batch_size, True)
-            shared.append(fps)
-        for num_apps in xs:
-            fps = inference.benchmark(dataset_dir, num_apps, batch_size, False)
-            not_shared.append(fps)
-    except:
-        total_completed = max(len(shared), len(not_shared))
-        xs = xs[:total_completed]
-        shared = shared[:total_completed]
-        not_shared = not_shared[:total_completed]
+    for num_apps in xs:
+        fps = inference.benchmark(data, num_apps, batch_size, False)
+        not_shared.append(fps)
+        fps = inference.benchmark(data, num_apps, batch_size, True)
+        shared.append(fps)
     print "[OUTPUT] Shared:", shared
     print "[OUTPUT] Not shared:", not_shared
     output_file = output_dir + "/" + "toplayer-bs" + str(batch_size) + "-" + str(num_apps_range) + "-" + str(num_apps_stride)
@@ -26,7 +22,6 @@ def run(dataset_dir, batch_size, num_apps_range, num_apps_stride, output_dir):
             s = str.format("{0:.4f}", s)
             line = str(x) + "," + str(ns) + "," + str(s) + "\n"
             f.write(line)
-
 
 
 if __name__ == "__main__":
