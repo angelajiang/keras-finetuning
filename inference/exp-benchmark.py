87028c0fa8a6574262d2e2d7a8a1cb8333f5e1a0
Angela Hao Jiang <ahjiang@h0.tl-1.biglearning.orca.pdl.cmu.edu>
2017-04-24 12:51:27 -0600

    WIP benchmarking with variable layers

--- inference/exp-benchmark.py
+++ inference/exp-benchmark.py
@@ -9,20 +9,19 @@ def run(dataset_dir, batch_size, num_apps_range, num_apps_stride, output_dir):
     shared = []
     not_shared = []
     for num_apps in xs:
-        fps = inference.benchmark(data, num_apps, batch_size, False)
-        not_shared.append(fps)
         fps = inference.benchmark(data, num_apps, batch_size, True)
         shared.append(fps)
+        #fps = inference.benchmark(data, num_apps, batch_size, False)
+        #not_shared.append(fps)
     print "[OUTPUT] Shared:", shared
     print "[OUTPUT] Not shared:", not_shared
-    output_file = output_dir + "/" + "toplayer-bs" + str(batch_size) + "-" + str(num_apps_range) + "-" + str(num_apps_stride)
+    output_file = output_dir + "/" + "varlayer-bs" + str(batch_size) + "-" + str(num_apps_range) + "-" + str(num_apps_stride)
     with open(output_file, "w") as f:
         for x, ns, s in zip(xs, not_shared, shared):
             ns = str.format("{0:.4f}", ns)
             s = str.format("{0:.4f}", s)
             line = str(x) + "," + str(ns) + "," + str(s) + "\n"
             f.write(line)
-
 
 if __name__ == "__main__":
     dataset_dir, batch_size, num_apps_range, num_apps_stride, output_dir = sys.argv[1:]
